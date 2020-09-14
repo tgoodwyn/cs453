f60806d86bb09a84837d72a9aa1799d94785c093
tgoodwyn <tgoodwyn@gmail.com>
2020-09-14 19:48:49 -0400

    lots of updates, see comments

--- hw1/index.php
+++ hw1/index.php
@@ -1,4 +1,5 @@
 <?php 
-  $mydata = $_REQUEST["mydata"];
-  print "<p>Hi, $mydata! This is new important data for your web page.</p> ";
-?>
+  // $mydata = $_REQUEST["mydata"];
+  // print "<p>Hi, $mydata! This is new important data for your web page.</p> ";
+  print "<p>Hi, ! This is new important data for your web page.</p> ";
+  ?>

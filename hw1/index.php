bb13d0a7f791efd521ec1ed632f803793605d8ea
tgoodwyn <tgoodwyn@gmail.com>
2020-09-15 18:29:27 -0400

    Finished a bunch of name changes , now to see if they work

--- hw1/index.php
+++ hw1/index.php
@@ -2,13 +2,13 @@
   // $occupied    = filter_var($_REQUEST["occupied"], FILTER_VALIDATE_BOOLEAN);
   $occupied    = $_REQUEST["occupied"];
   $StudentOrTeacher  = ($_REQUEST["StudentOrTeacher"]);
-  $DeskOrNot   = ($_REQUEST["DeskOrNot"]);
+  $NoDeskOrYes   = ($_REQUEST["NoDeskOrYes"]);
   $EntryOrExit = ($_REQUEST["EntryOrExit"]);
 
 
   echo "occupied    = $occupied   \n";
   echo "StudentOrTeacher  = $StudentOrTeacher \n";
-  echo "DeskOrNot   = $DeskOrNot  \n";
+  echo "NoDeskOrYes   = $NoDeskOrYes  \n";
   echo "EntryOrExit = $EntryOrExit\n";
   // print "<p>Hi, $mydata! This is new important data for your web page.</p> ";
   // if (!$occupied && $StudentOrTeacher)

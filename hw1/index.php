669b20034439721d05624bdeb47ad52a572bd5ea
tgoodwyn <tgoodwyn@gmail.com>
2020-09-15 15:28:44 -0400

    next steps: more info passed in query string, reporting logic on server side

--- hw1/index.php
+++ hw1/index.php
@@ -1,14 +1,4 @@
 <?php 
-<<<<<<< HEAD
-  $occupied = filter_var($_REQUEST["occupied"], FILTER_VALIDATE_BOOLEAN);
-  // print "<p>Hi, $mydata! This is new important data for your web page.</p> ";
-  if (!$occupied)
-  {
-    echo "<p>$occupied desk now full.</p> ";
-  } else {
-    echo "<p>desk already full.</p> ";
-  }
-=======
   // $occupied    = filter_var($_REQUEST["occupied"], FILTER_VALIDATE_BOOLEAN);
   $occupied    = $_REQUEST["occupied"];
   $StudentOrTeacher  = ($_REQUEST["StudentOrTeacher"]);
@@ -30,5 +20,4 @@
   //   echo $StudentOrTeacher;
 
   // }
->>>>>>> tb2
   ?>

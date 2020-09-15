eeca704739996ed4bb2c12ee5e4373d073dee623
tgoodwyn <tgoodwyn@gmail.com>
2020-09-15 15:08:00 -0400

    lots more working, now on to php query string part 2

--- hw1/index.php
+++ hw1/index.php
@@ -1,15 +1,22 @@
 <?php 
-  $occupied = filter_var($_REQUEST["occupied"], FILTER_VALIDATE_BOOLEAN);
-  $personType = ($_REQUEST["personType"]);
+  $occupied    = filter_var($_REQUEST["occupied"], FILTER_VALIDATE_BOOLEAN);
+  $personType  = ($_REQUEST["StudentOrTeacher"]);
+  $DeskOrNot   = ($_REQUEST["DeskOrNot"]);
+  $EntryOrExit = ($_REQUEST["EntryOrExit"]);
 
+
+  echo "occupied    = $occupied   ";
+  echo "personType  = $personType ";
+  echo "DeskOrNot   = $DeskOrNot  ";
+  echo "EntryOrExit = $EntryOrExit";
   // print "<p>Hi, $mydata! This is new important data for your web page.</p> ";
-  if (!$occupied && $personType)
-  {
-    echo "desk now full";
-    echo $personType;
-  } else {
-    echo "desk already full";
-    echo $personType;
+  // if (!$occupied && $personType)
+  // {
+  //   echo "desk now full";
+  //   echo $personType;
+  // } else {
+  //   echo "desk already full";
+  //   echo $personType;
 
-  }
+  // }
   ?>
